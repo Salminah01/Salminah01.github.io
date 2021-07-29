e9bcc888d017cd857602f60b1ef1717ae329304d
Salminah01 <88158158+Salminah01@users.noreply.github.com>
2021-07-29 19:55:34 +0200

    Add files via upload

---
+++ u7x8o/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Boot from "./Boot";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Boot />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
